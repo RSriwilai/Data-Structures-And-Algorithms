005782db5692385452e7c4cfaa78c11405355291
Rattanachai Sriwilai <duk151@hotmail.com>
2021-12-28 02:35:19 +0100

    Add project files.

---
+++ Data-Structures-And-Algorithm/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace Data_Structures_And_Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = { 9, 1, 8, 2, 7, 3, 6, 4, 5, 10 };
+
+            Sorting.Insertion(array);
+            foreach (int i in array)
+            {
+                Thread.Sleep(250);
+                Console.Write(i);
+            }
+
+            //Console.WriteLine(Sorting.IterativeFactorial(5));
+            //Console.WriteLine(Sorting.RecursionFactorial(55));
+        }
+
+        public static bool IsPalindrome(string input)
+        {
+            string str = RemoveNonAlphanumericCharacter(input.ToLower());
+            if (str == "")
+                return true;
+
+            for (int i = 0; i < str.Length / 2; i++)
+            {
+                if (str[i] != str[str.Length - i - 1])
+                    return false;
+            }
+            return true;
+        }
+
+        private static string RemoveNonAlphanumericCharacter(string str)
+        {
+            str = Regex.Replace(str, "[^a-zA-Z0-9]", String.Empty);
+            Console.WriteLine(str);
+            return str;
+        }
+    }
+}
---
+++ Data-Structures-And-Algorithm/Sorting.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_And_Algorithm
+{
+    public class Sorting
+    {
+        public static void BubbleSort(int[] arr)
+        {
+            for (int partIndex = arr.Length - 1; partIndex > 0; partIndex--)
+            {
+                for (int i = 0; i < partIndex; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        Swap(arr, i, i + 1);
+                    }
+                }
+            }
+        }
+
+        public static void SelectionSort(int[] arr)
+        {
+            for (int partIndex = arr.Length - 1; partIndex > 0; partIndex--)
+            {
+                int largestAt = 0;
+                for (int i = 1; i <= partIndex; i++)
+                {
+                    if (arr[i] > arr[largestAt])
+                    {
+                        largestAt = i;
+                    }
+                }
+                Swap(arr, largestAt, partIndex);
+            }
+        }
+
+        public static void InsertionSort(int[] arr)
+        {
+            //all the elements from the left side are considered sortet and the right side is unsorted
+            for (int partIndex = 1; partIndex < arr.Length; partIndex++)
+            {
+                int curUnsorterd = arr[partIndex];
+                int i = 0;
+                for (i = partIndex; i > 0 && arr[i - 1] > curUnsorterd; i--)
+                {
+                    arr[i] = arr[i - 1];
+                }
+                arr[i] = curUnsorterd;
+            }
+        }
+
+        public static void ShellSort(int[] arr)
+        {
+
+        }
+
+        private static void Swap(int[] array, int i, int j)
+        {
+            if (i == j)
+                return;
+
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+
+        public static int IterativeFactorial(int num)
+        {
+            if (num == 0)
+                return 1;
+
+            int factorial = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+
+        public static long RecursionFactorial(long n)
+        {
+            if (n == 0)
+                return 1;
+
+            return n * RecursionFactorial(n - 1);
+        }
+
+    }
+}
